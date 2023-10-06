578f5b717d08376503a4457c4c43e9c632e8ff31
MrLad01 <ladayodeji@gmail.com>
2023-10-06 17:51:16 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
-
+import product from "./assets/image-product-desktop.jpg"
 
 
 function App() {
-
-
   return (
-    <main>
+    <main className="h-screen w-screen flex justify-center items-center">
+      <div className="w-2/5 h-1/2 grid grid-cols-2">
+        <div>
+          <img src={product} alt="Gabrielle Essense eau de perfaum" className="object-contain rounded-s-xl" />
+        </div>
+        <div className="bg-black rounded-e-xl">
+        <h1 className="text-center font-bold text-blue-700">Hello World!</h1>
+        </div>
 
+      </div>
     </main>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/App.tsx"
+  ],
   theme: {
     extend: {},
   },
